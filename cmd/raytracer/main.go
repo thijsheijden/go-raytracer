661d1464b9674d449e7f230a0bd8b7c01df9dd1a
thijsheijden <hi@thijsheijden.nl>
2021-12-15 16:26:47 +0100

    Basic concurrency added, which is slower than single threaded....

--- cmd/raytracer/main.go
+++ cmd/raytracer/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"image"
+	"image/draw"
 	"image/jpeg"
+	"log"
 	"math"
 	"math/rand"
 	"os"
@@ -12,6 +14,8 @@ import (
 	"raytracer/internal/ray"
 	"raytracer/internal/scene"
 	"raytracer/internal/vector"
+	"runtime/pprof"
+	"sync"
 	"time"
 
 	_ "image/jpeg" // Needed for JPEG decoder
@@ -19,8 +23,16 @@ import (
 
 var loadedScene scene.Scene
 var nPixelSamples = 100
-var maxDepth = 20
+var maxDepth = 50
 var infinity = math.Inf(1)
+
+// Number of threads to split the work up
+const nThreads = 4
+
+type imagePart struct {
+	index, height int
+	image         *image.RGBA
+}
 
 func main() {
 	// Load in a scene file
@@ -54,40 +66,83 @@ func main() {
 	var vertical = vector.Vector{0, viewportHeight, 0}
 	var lowerLeftCorner = origin.Sub(horizontal.Scale(0.5)).Sub(vertical.Scale(0.5)).Sub(vector.Vector{0, 0, focalLength})
 
-	image := image.NewRGBA(image.Rect(0, 0, imageWidth, imageHeight))
+	const rowsPerThread = imageHeight / nThreads
+	const rest = imageHeight - (nThreads * rowsPerThread)
 
-	// Cast rays for each of the image pixels
-	for y := imageHeight - 1; y >= 0; y-- {
-		for x := 0; x < imageWidth; x++ {
-			// Define a new color for this pixel, which we will average later
-			var pixelColor color.RGB = color.New(0, 0, 0)
+	cpuProfile, err := os.Create("profile.pprof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pprof.StartCPUProfile(cpuProfile)
+	defer pprof.StopCPUProfile()
 
-			// Anti-aliasing
-			for i := 0; i < nPixelSamples; i++ {
-				var u float64 = (float64(x) + rand.Float64()) / (imageWidth + 1)
-				var v float64 = (float64(y) + rand.Float64()) / float64(imageHeight+1)
-				r := ray.New(origin, lowerLeftCorner.Add(horizontal.Scale(u)).Add(vertical.Scale(v)).Sub(origin))
-				pixelColor = pixelColor.Add(colorRay(r, maxDepth))
+	var wg sync.WaitGroup
+	imagePartChan := make(chan imagePart, nThreads)
+	for thread := 0; thread < nThreads; thread++ {
+		// If this is the last thread, it gets the remaining rows from incomplete division
+		startY := thread * rowsPerThread
+		endY := (thread + 1) * rowsPerThread
+		if thread == nThreads-1 {
+			endY += rest
+		}
+
+		// Join waitgroup
+		wg.Add(1)
+
+		go func(thread, startY, endY int, imagePartChan chan imagePart) {
+			defer wg.Done()
+			// Create slice for image part
+			img := image.NewRGBA(image.Rect(0, 0, imageWidth, (endY - startY)))
+
+			// Cast rays for each of the image pixels
+			for y := startY; y <= endY; y++ {
+				for x := 0; x < imageWidth; x++ {
+					// Define a new color for this pixel, which we will average later
+					var pixelColor color.RGB = color.New(0, 0, 0)
+
+					// Anti-aliasing
+					for i := 0; i < nPixelSamples; i++ {
+						var u float64 = (float64(x) + rand.Float64()) / (imageWidth + 1)
+						var v float64 = (float64(y) + rand.Float64()) / float64(imageHeight+1)
+						r := ray.New(origin, lowerLeftCorner.Add(horizontal.Scale(u)).Add(vertical.Scale(v)).Sub(origin))
+						pixelColor = pixelColor.Add(colorRay(r, maxDepth))
+					}
+
+					// Set pixel in slice
+					img.Set(x, endY-y, pixelColor.Average(nPixelSamples).RGBA())
+				}
 			}
 
-			// Set pixel in the image
-			image.Set(x, imageHeight-y, pixelColor.Average(nPixelSamples).RGBA()) // Flipping y coordinates
-		}
+			// Thread is done
+			imagePartChan <- imagePart{index: nThreads - thread - 1, height: endY - startY, image: img}
+
+		}(thread, startY, endY, imagePartChan)
+	}
+
+	// Wait for all threads to finish
+	wg.Wait()
+	close(imagePartChan)
+
+	// Stitch all image parts together
+	fullImage := image.NewRGBA(image.Rect(0, 0, imageWidth, imageHeight))
+	for part := range imagePartChan {
+		r := image.Rect(0, part.index*rowsPerThread, imageWidth, part.index*rowsPerThread+part.height)
+		draw.Draw(fullImage, r, part.image, image.ZP, draw.Src)
 	}
 
 	// Save image
-	f, err = os.Create("outimage.jpg")
+	output, err := os.Create("outimage.jpg")
 	if err != nil {
 		// Handle error
 	}
-	defer f.Close()
+	defer output.Close()
 
 	// Specify the quality, between 0-100
 	// Higher is better
 	opt := jpeg.Options{
 		Quality: 90,
 	}
-	err = jpeg.Encode(f, image, &opt)
+	err = jpeg.Encode(output, fullImage, &opt)
 	if err != nil {
 		// Handle error
 	}
